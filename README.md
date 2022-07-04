85f9bcf8f63c585d2014603db38287b5a1169a5a
jeet shekhaliya <51267357+whitestorm007@users.noreply.github.com>
2022-07-04 10:22:37 +0530

    first upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# jwt-
+
+
+<a href="https://heroku.com/deploy?template=https://github.com/whitestorm007/jwtsigma">
+  <img src="https://www.herokucdn.com/deploy/button.svg" alt="Deploy">
+</a>
---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express()
+const PORT = process.env.PORT || 4000
+
+const jwt = require('jsonwebtoken');
+const jwt_token = "SigmaSearchIsBetterThenAll"
+
+app.post("/validateStreamUrl", (req, res) => {
+    let token = req.query.token
+    if (token === undefined || token === null || token.trim() === "") {
+        res.json({ code: false })
+        return
+    }
+    let decode = jwt.decode(token, jwt_token)
+    let final = (decode !== null) ? res.json({ code: true, data: decode.data }) : res.json({ code: false });
+});
+
+app.listen(PORT, () => {
+    console.log(`server is running on port http://localhost:${PORT}`);
+});
